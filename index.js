488cc59fbedc4770f8933516cf638122a40365df
Kapatych <onetruestory@ya.ru>
2020-07-07 16:27:21 +0300

    Second commit

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 let state = {
   todos: [
-    {id: 1, title: 'First todo', status: 'Open'},
-    {id: 2, title: 'Second todo', status: 'In progress'},
-    {id: 3, title: 'Third todo', status: 'Open'},
-    {id: 4, title: 'Fourth todo', status: 'Close'}
+    { id: 1, title: "First todo", status: "Open" },
+    { id: 2, title: "Second todo", status: "In progress" },
+    { id: 3, title: "Third todo", status: "Open" },
+    { id: 4, title: "Fourth todo", status: "Close" },
   ],
-  filter: ''
+  filter: "",
+  search: "",
 };
 
-const todoList = document.querySelector('.todo-list');
-const todoForm = document.querySelector('.todo-form');
-const todoInput = todoForm.querySelector('.todo-input');
-const modal = document.querySelector('.modal');
-const filterButtons = document.querySelectorAll('.filter');
-const instance = M.Modal.init(modal);
+const todoList = document.querySelector(".todo-list");
+const todoForm = document.querySelector(".todo-form");
+const todoInput = todoForm.querySelector(".todo-input");
+const modal = document.querySelector(".modal");
+const modalInstance = M.Modal.init(modal);
+const filterButtons = document.querySelectorAll(".filter");
+const searchInput = document.querySelector(".search-input");
 
-filterButtons.forEach(btn => btn.addEventListener('click', (e) => filterTodos(e.target.value)))
+const renderTodo = ({ title, status }) => {
+  let container = document.createElement("li");
+  container.classList.add("collection-item");
+  container.classList.add("row");
+  container.insertAdjacentHTML(
+    "beforeend",
+    `<span class="col s9">${title}</span>`
+  );
 
-const renderTodo = ({title, status}) => {
-  let container = document.createElement('li');
-  container.classList.add('collection-item');
-  container.classList.add('row');
-  container.insertAdjacentHTML('beforeend',
-    `<span class="col s9">${title}</span><span class="col s2">${status}</span>`);
-  container.insertAdjacentHTML('beforeend',
-    `<a class="waves-effect waves-light btn remove col s1"><i class="material-icons">delete</i></a>`);
+  const selects = ["Open", "In progress", "Close"].map(
+    (st) =>
+      `<option value="${st}" ${status === st ? "selected" : ""}>${st}</option>`
+  );
+
+  container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="input-field col s2">
+    <select> ${selects.join("")} </select>
+    <label>Status</label>
+  </div>`
+  );
+  container.insertAdjacentHTML(
+    "beforeend",
+    `<a class="waves-effect waves-light btn remove col s1"><i class="material-icons">delete</i></a>`
+  );
   return container;
 };
 
@@ -38,13 +55,38 @@ const filterTodos = (value) => {
   updateTodos();
 };
 
-const renderTodos = () => {
-  state.todos.filter(el => el.status.includes(state.filter)).forEach(el => todoList.append(renderTodo(el)));
-  const removeButtons = document.querySelectorAll('.remove');
-  removeButtons.forEach((btn, idx) => btn.addEventListener('click', () => removeHandler(idx)));
+const searchTodo = (value) => {
+  state.search = value;
+  updateTodos();
 };
 
-const clearTodos = () => todoList.querySelectorAll('.collection-item').forEach(el => el.remove());
+const changeStatusHandler = (value, idx) => {
+  state.todos[idx].status = value;
+  updateTodos();
+};
+
+const renderTodos = () => {
+  state.todos
+    .filter((el) => el.status.includes(state.filter))
+    .filter((el) => el.title.toLowerCase().includes(state.search.toLowerCase()))
+    .forEach((el) => todoList.append(renderTodo(el)));
+
+  const removeButtons = document.querySelectorAll(".remove");
+  removeButtons.forEach((btn, idx) =>
+    btn.addEventListener("click", () => removeHandler(idx))
+  );
+
+  const selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+  selects.forEach((s, idx) =>
+    s.addEventListener("change", (e) =>
+      changeStatusHandler(e.target.value, idx)
+    )
+  );
+};
+
+const clearTodos = () =>
+  todoList.querySelectorAll(".collection-item").forEach((el) => el.remove());
 
 const updateTodos = () => {
   clearTodos();
@@ -56,15 +98,19 @@ const submitHandler = (e) => {
   const newTodo = {
     id: state.length + 1,
     title: todoInput.value,
-    status: 'Open'
+    status: "Open",
   };
   state.todos.push(newTodo);
   updateTodos();
   todoInput.blur();
   todoForm.reset();
-  instance.close()
+  modalInstance.close();
 };
 
-todoForm.addEventListener('submit', (e) => submitHandler(e));
-renderTodos();
+todoForm.addEventListener("submit", (e) => submitHandler(e));
+searchInput.addEventListener("change", (e) => searchTodo(e.target.value));
+filterButtons.forEach((btn) =>
+  btn.addEventListener("click", (e) => filterTodos(e.target.value))
+);
 
+renderTodos();
